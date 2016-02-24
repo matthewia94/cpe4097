b0a101927b9a0c75ee71c3f53aa040593e47da62
Matt Anderson <matthewia94@gmail.com>
2016-02-24 23:21:09 +0000

    Remove print statements
    The print statements will slow down the stepping and so have been
    removed.

--- GPIOClass.cpp
+++ GPIOClass.cpp
@@ -22,10 +22,8 @@ int GPIOClass::export_gpio() //Exports GPIO to Pi for use
 	ofstream exportgpio(export_str.c_str());
 	if(exportgpio < 0)
 	{
-		cout << "Failed to export GPIO" << this->gpionum << endl;
 		return -1;
 	}
-	cout << "export string of "<< this->gpionum << ": " << export_str.c_str() << endl;
 	exportgpio << this->gpionum;
 	exportgpio.close();
 	return 0;
@@ -36,11 +34,9 @@ int GPIOClass::unexport_gpio() //Frees GPIO pin after use
 	string unexport_str = "/sys/class/gpio/unexport";
 	ofstream unexportgpio(unexport_str.c_str());
 	if(unexportgpio < 0)
-	{
-		cout << "Unable to unexport GPIO" << this->gpionum << endl;
+    {
 		return -1;
 	}
-	cout << "unexport string of "<< this->gpionum << ": " << unexport_str.c_str() << endl;
 	unexportgpio << this->gpionum;
 	unexportgpio.close();
 	return 0;
@@ -52,11 +48,9 @@ int GPIOClass::setdir_gpio(string dir) //Sets pin to input or output
 	ofstream setdirgpio(setdir_str.c_str());
 	if(setdirgpio < 0)
 	{
-		cout << "Unable to set GPIO direction of GPIO" << this->gpionum << endl;
 		return -1;
 	}
 	setdirgpio << dir;
-	cout << "setdir string of "<< this->gpionum << ": " << setdir_str.c_str() << endl;
 	setdirgpio.close();
 	return 0;
 }
@@ -64,15 +58,12 @@ int GPIOClass::setdir_gpio(string dir) //Sets pin to input or output
 int GPIOClass::setval_gpio(string val) //Sets pin to high or low state
 {
 	string setval_str = "/sys/class/gpio/gpio" + this->gpionum + "/value";
-	cout << setval_str << endl;
 	ofstream setvalgpio(setval_str.c_str());
 	if(setvalgpio < 0)
 	{
-		cout << "Unable to set value of GPIO" << this->gpionum << endl;
 		return -1;
 	}	
 	setvalgpio << val;
-	cout << "Setval string of "<< this->gpionum << ": " << setval_str.c_str() << endl;
 	setvalgpio.close();
 	return 0;
 }
@@ -83,7 +74,6 @@ int GPIOClass::getval_gpio(string& val) //Reads value of GPIO pin
 	ifstream getvalgpio(getval_str.c_str());
 	if(getvalgpio < 0)
 	{
-		cout << "Unable to get value of GPIO" << this->gpionum << endl;
 		return -1;
 	}
 	
@@ -93,7 +83,6 @@ int GPIOClass::getval_gpio(string& val) //Reads value of GPIO pin
 		val = "1";
 	else
 		val = "0";
-	cout << "Getval string of "<< this->gpionum << ": " << getval_str.c_str() << endl;
 	getvalgpio.close();
 	return 0;
 }
